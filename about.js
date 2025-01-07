890516d5a14b33eaebf2a965cfe56df961e001bf
Yadlapalli Phanindra <160891993+manofbrilliant@users.noreply.github.com>
2025-01-07 18:31:52 +0530

    Files

---
+++ about.js
@@ -0,0 +1,9 @@
+// JavaScript to add fixed navbar functionality
+window.onscroll = function() {
+    var navbar = document.getElementById("navbar");
+    if (window.pageYOffset > 50) {
+        navbar.classList.add("fixed");
+    } else {
+        navbar.classList.remove("fixed");
+    }
+};
